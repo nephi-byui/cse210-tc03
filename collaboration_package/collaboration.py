da8e156943d01498283664bf2197434dd7fe763b
Alan Crisanto <alanvcrisanto@gmail.com>
2021-10-02 09:30:37 -0500

    Edit Poem

--- collaboration_package/collaboration.py
+++ collaboration_package/collaboration.py
@@ -14,3 +14,4 @@ print()
 print('There was once a programmer with code') # nephi
 print("His dream was to make a great change in the world")# Felix
 print("He was not a hacker") #Tianna
+print("But he was a dreamer") #Alan
